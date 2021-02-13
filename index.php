9fc521397ea8f435ba22a3547f9e6b0c0c90518a
Jan Babák <61386198+babakjan@users.noreply.github.com>
2021-02-13 11:59:01 +0100

    make C matrix input fields readonly

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ function multiplication()
                 <?php
                 for ($i = 0; $i < 3; $i++) {
                     for ($j = 0; $j < 3; $j++) {
-                        echo "<input type=\"text\"  value=\"";
+                        echo "<input type=\"text\" readonly value=\"";
                         echo ($C !== []) ? $C[$i][$j] : "0";
                         echo "\">";
                     }
